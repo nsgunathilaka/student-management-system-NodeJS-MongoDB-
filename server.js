116df1d32df0de3b302844eb53b4b8ea5cb59fe6
nihalgunathilaka <nihalshantha470@gmail.com>
2022-11-12 05:05:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+   // origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+   origin: "*",
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
